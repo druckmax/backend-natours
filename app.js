be3d98866845133447d75872e8cfe337c43be204
druckmax <max_sommerfeld@web.de>
2022-10-02 17:57:53 +0200

    Sets up esLint

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
-const tourRouter = require("./routes/tourRoutes.js");
-const userRouter = require("./routes/userRoutes.js");
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 //// 1) MIDDLEWARES
 // Middleware: Function that can modifiy the incoming requesst data
 // Third party middleware: Morgan is a logger middleware
-app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 // Internal middlewares
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
 // Custom middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -20,7 +24,7 @@ app.use((req, res, next) => {
 
 //// 2) ROUTES
 // This is called mounting the router
-app.use("/api/v1/tours", tourRouter);
-app.use("/api/v1/users", userRouter);
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-const app = require("./app.js");
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log("Listening on port " + port));
+app.listen(port, () => console.log('Listening on port ' + port));
