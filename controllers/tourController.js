1d64c3ad9cde1b16f8048bb74dacd0df7ab90bc3
druckmax <max_sommerfeld@web.de>
2022-10-05 20:30:56 +0200

    Starts implementing search queries for all tours, Hides .config.env, Fixes pathes in server and import-dev-data

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,33 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    // Getting all the tours in the database with find method and no arguments
-    const tours = await Tour.find();
+    // Added argument for query search, classic mongoDB way
+    // const tours = await Tour.find({
+    //   duration: 5,
+    //   difficulty: 'easy',
+    // });
+    // Mongoose way to filter the results
+    /* 
+      const tours = await Tour.find()
+      .where('duration')
+      .equals(5)
+      .where('difficulty')
+      .equals('easy');
+    */
+    // Since the query object we get from req.query looks exactly the same as the one we pass in above we can simply pass in req.query
+    // First we need to create a shallow copy of req.query because we want only certain queries to take effect (for example we do not want the pagination query (?page=2) for our filter)
+    // We create an array of all the fields we want to exclude from the query object
+    // We remove all the fields we do not want from the shallow copy of req.query with a forEach loop
+    // Now we can use the queryObject in the find method to get the tours filtered for the specified queries
+    // Find will return a query object which allows to append other methods right to it like sort() or where().
+    // But as soon as we are using await in our find method, which returns the query object, the query will execute and come back with the documents that match the query, which makes it impossible for us to later implement sorting or pagination methods by appending them. Instead we need to save the query as an actual query without the await and then await this query in a separate line of code.
+
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    const query = Tour.find(queryObj);
+    const tours = await query;
 
     // Sending JSON back and format it according to Jsend specification
     // Additionally adding results key which holds the length of the tours array as an extra
