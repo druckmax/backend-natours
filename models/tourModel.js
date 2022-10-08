7c7ee33076b860443dac67377ae3cc3b3e04e5d2
druckmax <max_sommerfeld@web.de>
2022-10-08 21:09:48 +0200

    Introduces query middleware, Creates secret tour and hides it from output wit pre('find')

--- models/tourModel.js
+++ models/tourModel.js
@@ -60,6 +60,10 @@ const tourSchema = new mongoose.Schema(
     },
     startDates: [Date],
     slug: String,
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   // OPTIONS OBJECT OF THE SCHEMA
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
@@ -89,7 +93,7 @@ tourSchema.virtual('durationWeeks').get(function () {
 // DOCUMENT MIDDLEWARE
 /* Document Middleware can act on the currently processed document. Just like virtual properties, we define middleware in the schema.
 
-The document middleware can run on validate, save, remove, updateOne, deleteOne, init and create, as create fires save() hooks. It does not run on insertMany, which is done via the model middleware.
+The document middleware can run on validate, save, remove, updateOne, deleteOne, init and create, as create fires save() hooks. It does not run on insertMany, which is done via the model middleware. The most commmon event trigger is 'save'.
 
 In this example we use a pre hook in order to run a function before a document is actually saved to the database. This allows us to act on the data before it is passed on. Here, we use it to create slug for the current document with slugify. Quick reminder: A slug is a part of an URL which consists out of a single or multiple words, often separated by hyphens, which are easy to understand for users and search engines. A slug is basically just a string that we can put in the URL, usually based on something like the name of a document.
 
@@ -105,10 +109,33 @@ tourSchema.pre('save', function (next) {
 /* The post hook additionally has access to the document object, which in case of 'save' is the document we just saved to our database. Like the name implies, post middleware functions are executed after all the pre middlewre functions finished executing. In the post hook we no longer have the this keyword, but the final document in the doc object.
  */
 
-tourSchema.post('save', function (doc, next) {
-  console.log(doc);
+// tourSchema.post('save', function (doc, next) {
+//   console.log(doc);
+//   next();
+// });
+
+// QUERY MIDDLEWARE
+/* Query middleware allows us to run functions befoe or after a certain query is executed.
+
+While post and pre hooks look identical in terms of syntax, the difference of the query middleware and the document middleware lies in the this keyword. When we define the 'find' trigger in the pre hook, the this keyword will now point to the current query object.
+
+In this example we create a pre-find hook, which runs before any find query is executed. A use case for this could be if we wanted to have secret tours, that are only offered internally or are only accessible for a certain group of people. We therefore create a secretTour field in our schema. Now we use the find method in the pre hook and select all the tours where the secretTour field is not equal to true. This way we also select all the documents that do not have the secretTour property. By doing this we basically filter out all the secret tours from the output.
+ NOTE: Mongoose actually creates the secretTour field for all the existing documents and sets it to its default, so {secetTour: false} would also work.
+
+A problem that occurs that the 'find' trigger does not work for findOne (also not findById which actually uses findOne). We could simply create another pre hook with findOne, but it is a better practice to use a regular expression in this case. We do this with /^find/, which means it should select all the strings that start with 'find'.
+*/
+// tourSchema.pre('find', function (next) {
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
   next();
 });
+
+/* In the post hook of the query middleware we get access to all the documents that are returned from the query, since this middleware runs after the query has finished its execution.
+ */
+// tourSchema.post(/^find/, function (docs, next) {
+//   console.log(docs);
+//   next();
+// });
 
 // Convention to put model variable with capital letter
 const Tour = mongoose.model('Tour', tourSchema);
