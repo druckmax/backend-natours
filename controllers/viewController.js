5515fb0cdf426b408bcf69863d8480fb91d89750
druckmax <max_sommerfeld@web.de>
2022-11-10 15:54:58 +0100

    Creates dynamic render of templates for overview route

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1. Gettour data from collection
+  const tours = await Tour.find();
+  // 2. Build tempalte
+
+  // 3. Render template using tour data from step 1)
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,15 +3,7 @@ const { getOverview, getTour } = require('../controllers/viewController');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forst Hiker',
-    user: 'Jonas',
-  });
-});
-
-router.get('/overview', getOverview);
-
+router.get('/', getOverview);
 router.get('/tour', getTour);
 
 module.exports = router;
